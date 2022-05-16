31407a284bd6ac85d7fb627d7d97eb062404a2d8
lenovo <dirol-88@mail.ru>
2022-05-16 16:23:00 +0300

    fix  Error resolving

--- src/main/java/com/example/sneakersworld/config/ParsingNews.java
+++
@@ -1,9 +0,0 @@
-package com.example.sneakersworld.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ParsingNews {
-
-}
--- src/main/java/com/example/sneakersworld/controllers/HomeController.java
+++ src/main/java/com/example/sneakersworld/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 public class HomeController {
     @GetMapping("/")
     public String homePage(){
-        return"/home";
+        return"home";
     }
 
 }
